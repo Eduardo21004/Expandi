09f7cd0ef457af84826b2def81435f2c14d7781d
Eduardo21004 <euribegarcia0@gmail.com>
2023-11-15 21:44:06 -0500

    primer commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const panels = document.querySelectorAll(".panel");
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click',() => {
+
+        removeActivveClasses();
+        panel.classList.add('active')
+
+    })
+})
+
+function removeActivveClasses(){
+    panels.forEach(panel =>{
+        panel.classList.remove('active')
+
+    })
+}
